3c789d66d9aeb4dafaee8e532b7e60710c4cf94b
Brefkaa <brefkaa@gmatc.matc.edu>
2026-01-27 14:34:40 -0600

    Completed Activity 1

--- blog/gatsby-config.js
+++ blog/gatsby-config.js
@@ -9,10 +9,15 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Gatsby Title`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    contact: {
+      name: "Alex Brefka",
+      company: "Brefka Consulting",
+      address: "123 Main Street, Anytown, USA"
+    }
   },
   plugins: [
     `gatsby-plugin-image`,
